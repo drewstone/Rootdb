2bef22563b83b4d78288cc45ec673f209b17add0
Don Yu <dyu@seas.upenn.edu>
2016-03-21 22:01:47 -0500

    added roots model

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,6 @@ var express = require('express')
   , router = express.Router()
 
 var db = require('../db.js')
-var Index = require('../models/root.js')
+var Index = require('../models/roots.js')
 
 module.exports = router
